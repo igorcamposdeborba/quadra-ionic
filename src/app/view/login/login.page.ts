36cffd332fab7efa5bc1bd38f9dded0ac0c18e2f
igorcamposdeborba <igor_campos5@hotmail.com>
2023-12-09 00:40:55 -0300

    login backup implemented

--- src/app/view/login/login.page.ts
+++ src/app/view/login/login.page.ts
@@ -16,14 +16,13 @@ export class LoginPage {
 
   constructor(private loginService : LoginService, private router: Router) {}
 
-  sendForm() : void {
-    let isValid : boolean = this.validateRequiredLabels();
-
-    if (isValid){
+  sendForm(): void {
+    let isValid: boolean = this.validateRequiredLabels();
+  
+    if (isValid) {
       this.loginService.login(this.userName, this.password).subscribe(
         response => {
           if (response.typeUser.toUpperCase() === 'ADMIN') {
-
             LocalNotifications.schedule({
               notifications: [
                 {
@@ -34,19 +33,39 @@ export class LoginPage {
                 }
               ]
             });
-
             this.router.navigate(['/admin']);
           }
         }, error => {
-          this.message = `${error.error.message}`;
-
-          setTimeout(() => {
-            this.message = '';
-          }, 3000);
+          if (error.status === 0) { // checar erro de conexão com o back-end
+            // Backup de login
+            const errorLogin = {
+              id: 1,
+              name: 'Error User',
+              email: 'error@example.com',
+              password: 'errorPassword',
+              typeUser: 'ERROR'
+            };
+  
+            LocalNotifications.schedule({
+              notifications: [
+                {
+                  title: 'Erro de Conexão',
+                  body: 'Houve um erro de conexão. Usando credenciais de erro.',
+                  id: 2,
+                  schedule: { at: new Date(Date.now()) }
+                }
+              ]
+            });
+          } else {
+            this.message = `${error.error.message}`;
+            setTimeout(() => {
+              this.message = '';
+            }, 3000);
+          }
         }
       );
     } else {
-      this.message = 'Preencha com o tamanho mínimo correto o usuário e senha.'
+      this.message = 'Preencha com o tamanho mínimo correto o usuário e senha.';
     }
   }
 
